96dfba2b71b55ed0c80fd5f90ae0bea8e636d9df
Seth Orell <seth.orell@gmail.com>
2022-09-03 16:57:05 -0500

    Implement authorizer

--- src/authorizer/service.ts
+++ src/authorizer/service.ts
@@ -1,13 +1,39 @@
 import Logger from '@dazn/lambda-powertools-logger';
 import { PolicyDocument } from 'aws-lambda';
+import { CognitoJwtVerifier } from 'aws-jwt-verify';
+import jwt from 'jsonwebtoken';
 
-const getPolicy = async (event: any) => {
-  Logger.debug('In service.authorize()', { event });
+const getPolicy = async (input: { methodArn: string; authorizationToken: string }) => {
+  Logger.debug('In service.authorize()', { input }); // TODO: stop logging input --SRO
+  const { methodArn, authorizationToken } = input;
+  const clientId = getClientId(authorizationToken);
+  const verifier = CognitoJwtVerifier.create({
+    userPoolId: process.env.USER_POOL_ID ?? '',
+    clientId,
+    tokenUse: 'access',
+  });
+  try {
+    const payload = await verifier.verify(authorizationToken);
+    Logger.debug('Temporary trace: authorized', { payload });
 
-  return generatePolicy({ effect: 'Allow', resource: event.methodArn });
+    return generatePolicy({ effect: 'Allow', resource: methodArn });
+  } catch (err) {
+    Logger.debug('Temporary trace: unauthorized');
+
+    throw Error('Unauthorized'); // This becomes 401/Unauthorized in API Gateway
+  }
 };
 
-const generatePolicy = (input: { effect: string, resource: string }): PolicyDocument => {
+const getClientId = (authToken: string): string => {
+  const decoded: any = jwt.decode(authToken, { complete: true });
+  if (!decoded) {
+    throw new Error('Cannot parse authToken');
+  }
+
+  return decoded.payload.client_id;
+};
+
+const generatePolicy = (input: { effect: string; resource: string }): PolicyDocument => {
   const { effect, resource } = input;
   const policyDocument: PolicyDocument = {
     Version: '2012-10-17',
