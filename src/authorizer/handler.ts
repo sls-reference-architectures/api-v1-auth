2dab4dba006ca365a73f7ece5cc98e874352fd56
Seth Orell <seth.orell@gmail.com>
2022-09-03 13:05:11 -0500

    Return AuthResponse, not policy

--- src/authorizer/handler.ts
+++ src/authorizer/handler.ts
@@ -1,12 +1,17 @@
 import Logger from '@dazn/lambda-powertools-logger';
 import middy from '@middy/core';
+import { AuthResponse } from 'aws-lambda';
 
 import getPolicy from './service';
 
-const authorize = async (event: any) => {
+const authorize = async (event: any): Promise<AuthResponse> => {
   Logger.debug('In handler.authorize()', { event });
+  const policy = await getPolicy(event); // TODO - parse out what we need from event and pass it in
 
-  return getPolicy(event); // TODO - parse out what we need from event and pass it in
+  return {
+    principalId: 'wat',
+    policyDocument: policy,
+  }
 };
 
 export default middy(authorize);
--- src/hello/test/hello.e2e.test.ts
+++ src/hello/test/hello.e2e.test.ts
@@ -6,11 +6,16 @@ describe('When getting a hello', () => {
     const path = 'hello';
     const options: AxiosRequestConfig = {
       baseURL: process.env.API_URL,
+      headers: {
+        Authorization: 'Bearer abc',
+      },
+      validateStatus: () => true,
     };
+    console.log(options)
 
     // ACT
     const { status, data } = await axios.get(path, options);
-
+console.log(data)
     // ASSERT
     expect(status).toEqual(200);
     expect(data.message).toMatch(/hello world/i);
