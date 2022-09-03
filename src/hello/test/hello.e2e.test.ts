f8a5e5f7921f20e2deae2259c048269fbd4278cf
Seth Orell <seth.orell@gmail.com>
2022-09-03 13:13:36 -0500

    Add negative test

--- src/hello/test/hello.e2e.test.ts
+++ src/hello/test/hello.e2e.test.ts
@@ -1,22 +1,41 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 describe('When getting a hello', () => {
-  it('should return 200', async () => {
-    // ARRANGE
-    const path = 'hello';
-    const options: AxiosRequestConfig = {
-      baseURL: process.env.API_URL,
-      headers: {
-        Authorization: 'Bearer abc',
-      },
-      validateStatus: () => true,
-    };
+  describe('with a valid token', () => {
+    it('should return 200', async () => {
+      // ARRANGE
+      const path = 'hello';
+      const options: AxiosRequestConfig = {
+        baseURL: process.env.API_URL,
+        headers: {
+          Authorization: 'Bearer abc', // TODO: populate with valid token
+        },
+        validateStatus: () => true,
+      };
+  
+      // ACT
+      const { status, data } = await axios.get(path, options);
+  
+      // ASSERT
+      expect(status).toEqual(200);
+      expect(data.message).toMatch(/hello world/i);
+    });
+  });
 
-    // ACT
-    const { status, data } = await axios.get(path, options);
-
-    // ASSERT
-    expect(status).toEqual(200);
-    expect(data.message).toMatch(/hello world/i);
+  describe('without an auth token', () => {
+    it('should return 401', async () => {
+      // ARRANGE
+      const path = 'hello';
+      const options: AxiosRequestConfig = {
+        baseURL: process.env.API_URL,
+        validateStatus: () => true,
+      };
+  
+      // ACT
+      const { status, data } = await axios.get(path, options);
+  
+      // ASSERT
+      expect(status).toEqual(401);
+    });
   });
 });
