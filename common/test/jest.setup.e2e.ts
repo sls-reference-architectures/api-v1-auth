221c2e6d34cba05e30a94cd752d8c6e6d7f02a00
Seth Orell <seth.orell@gmail.com>
2022-09-04 12:32:06 -0500

    Fix e2e test setup

--- common/test/jest.setup.e2e.ts
+++ common/test/jest.setup.e2e.ts
@@ -1,5 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { getRestServiceEndpoint, getStack, getTestToken, getUserPoolId } from './setupUtils';
+
+import {
+  getStack,
+  getRestServiceEndpoint,
+  getUserPoolId,
+  getFullAccessTestToken,
+  getReadOnlyTestToken,
+} from './setupUtils';
 
 const region = process.env.AWS_REGION || 'us-east-1';
 const stage = process.env.STAGE || 'dev';
@@ -10,14 +17,16 @@ const setup = async (): Promise<void> => {
   const stack = await getStack(stackName);
   const serviceUrl = getRestServiceEndpoint(stack);
   const userPoolId = getUserPoolId(stack);
-  const { accessToken } = await getTestToken(stack);
+  const { accessToken: fullAccessToken } = await getFullAccessTestToken(stack);
+  const { accessToken: readAccessToken } = await getReadOnlyTestToken(stack);
 
   process.env.AWS_REGION = region;
   process.env.STAGE = stage;
   process.env.NODE_ENV = stage;
   process.env.API_URL = serviceUrl;
   process.env.USER_POOL_ID = userPoolId;
-  process.env.ACCESS_TOKEN = accessToken;
+  process.env.FULL_ACCESS_TOKEN = fullAccessToken;
+  process.env.READ_ACCESS_TOKEN = readAccessToken;
 };
 
 export default setup;
