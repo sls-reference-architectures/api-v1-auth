b8f85dfd3b27338d2d9f992aca755fea051a3095
Seth Orell <seth.orell@gmail.com>
2024-11-11 15:35:00 -0600

    A couple more

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -2,6 +2,6 @@ const jestConfig = require('./jest.config');
 
 module.exports = {
   ...jestConfig,
-  globalSetup: './common/test/jest.setup.e2e.ts',
+  globalSetup: './common/test/jest.setup.e2e.js',
   testTimeout: 600000,
 };
--- jest.config.int.js
+++ jest.config.int.js
@@ -2,6 +2,6 @@ const jestConfig = require('./jest.config');
 
 module.exports = {
   ...jestConfig,
-  globalSetup: './common/test/jest.setup.int.ts',
+  globalSetup: './common/test/jest.setup.int.js',
   testTimeout: 600000,
 };
