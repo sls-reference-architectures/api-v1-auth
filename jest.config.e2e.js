98fe096d6c14ee1dc29c39a6f6594261f2f1fff4
Seth Orell <seth.orell@gmail.com>
2025-06-17 08:58:49 -0500

    To five minutes

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -3,5 +3,5 @@ const jestConfig = require('./jest.config');
 module.exports = {
   ...jestConfig,
   globalSetup: './common/test/jest.setup.e2e.js',
-  testTimeout: 120000,
+  testTimeout: 300000,
 };
